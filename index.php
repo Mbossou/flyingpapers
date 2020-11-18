90292f09ff6a8de459448b96126d27debefdc3cd
Mélissa <melissa.bossou@protonmail.com>
2020-11-18 11:33:24 +0100

    Ajout pages
    HTML,CSS des pages principales, reste à rendre les pages responsive

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/script.js"></script>
+    
+    <title>Comparez plusieurs billets de transport et voyagez léger | Flying Papers</title>
+     
+</head>
+<body>
+    
+    <a href="#"></a>
+    <!-- Choix des langues en haut à droite -->
+    <!-- Menu : "Vos réservations" ; "S'inscrire" ; "Se connecter" en haut à droite -->
+    <nav class="navbar navbar-expand-md">
+        <a class="navbar-brand" href="#"><img src="img/logo.PNG" alt="logo Flying Papers" /></a>
+    
+          <div class="collapse navbar-collapse justify-content-right" id="menu">
+            <ul class="nav nav-pills navbar-nav">
+                <li class="nav-item"><a class="nav-link nav-link-style" href="#langue">Choix de la langue</a></li>
+                <li class="nav-item"><a class="nav-link nav-link-style" href="#reservations">Vos réservations</a></li>
+                <li class="nav-item"><a class="nav-link nav-link-style" href="#inscription">S'inscrire</a></li>
+                <li class="nav-item"><a class="nav-link bouton-bleu" href="#connexion">Se connecter</a></li>
+            </ul>
+          </div>
+      </nav>
+      
+    <section id="moteur-recherche">
+        <h1>Voyager l'esprit léger<span style="color:#f0856b">.</span> Tout simplement<span style="color:#f0856b">.</span></h1>
+        <h4>Flying Papers vous accompagne pour faciliter vos déplacements en France et en Europe tout en restant écolo !</h4>
+        <div class=container>
+            <div class="row">
+                
+                <?php
+                $lieu_depart1 = "Paris, France";
+                $lieu_depart2 = "Rome, Italie";
+                $lieu_depart3 = "Londres, Angleterre";
+                $lieu_arrivee1 = "Paris, France";
+                $lieu_arrivee2 = "Rome, Italie";
+                $lieu_arrivee3 = "Londres, Angleterre";
+                
+                echo '<form method="get" action="resultats.php">';
+                    echo '<input list="villesDepart" name="villeDepart" id="ville-depart-choix" type="text" placeholder="Ville de départ">';
+                        echo '<datalist id="villesDepart">';                 
+                            echo '<option value="'.$lieu_depart1.'">';                   
+                            echo '<option value="'.$lieu_depart2.'">';                   
+                            echo '<option value="'.$lieu_depart3.'">';                   
+                        echo '</datalist>';         
+                    echo '<input list="villesArrivee" name="villeArrivee" id="ville-arrivee-choix" type="text" placeholder="Ville d\'arrivée">';
+                        echo '<datalist id="villesArrivee">';                   
+                            echo '<option value="'.$lieu_arrivee1.'">';                   
+                            echo '<option value="'.$lieu_arrivee2.'">';                   
+                            echo '<option value="'.$lieu_arrivee3.'">';
+                        echo '</datalist>';         
+                        //query pour comparer les variables avec donnees de billets.ld_recherche et billets.la_recherche -> affichage conditionnel
+                    echo '<label for="dateDepart">Date de départ :</label>';
+                    echo '<input list="datesDepart"  name="dateDepart" id="dateDepart" type="date" placeholder="Date de départ">';
+                    echo '<label for="dateArrivee">Date de retour (Falcultatif) :</label>';
+                    echo '<input list="datesArrivee" name="dateArrivee" id="dateArrivee" type="date" placeholder="Ajouter un retour">';
+                    echo '<input type="SUBMIT" value="Rechercher" id="recherche">'; //ajouter un évènement sur submit : redirection vers page resultats.php
+                    //echo '<!-- <button type="submit" id="recherche" class="btn">Rechercher</button> -->';
+                echo '</form>';
+                ?>
+            </div>
+        </div>
+        <div style="background:pink;color:#333;position:fixed;right:0;bottom:0;z-index:99999999;font:1em arial;" id="ld"></div><script>setInterval(function(){if($(window).height()>=$(document).height()){$('#ld').text($(document).width()+' px');}else{$('#ld').text($(document).width()+17+' px');}},150);</script>
+
+    </section>
+</body>
+</html>
---
+++ resultats.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="js/script.js"></script>
+        
+        <title>Comparez plusieurs billets de transport et voyagez léger | Flying Papers</title>
+        
+    </head>
+    <body>
+        
+        <a href="#"></a>
+        <!-- Choix des langues en haut à droite -->
+        <!-- Menu : "Vos réservations" ; "S'inscrire" ; "Se connecter" en haut à droite -->
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <a  href="#"><img class="navbar-brand" src="img/logo.PNG" alt="logo Flying Papers" /></a>
+        
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="nav nav-pills navbar-nav">
+                    <li class="nav-item"><a class="nav-link nav-link-style" href="#langue">FR</a></li>
+                    <li class="nav-item"><a class="nav-link nav-link-style" href="#reservations">Vos réservations</a></li>
+                    <li class="nav-item"><a class="nav-link nav-link-style" href="#inscription">S'inscrire</a></li>
+                    <li class="nav-item"><a class="nav-link bouton-peche" href="#connexion">Se connecter</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="recaps-trajet col-12">
+            <div class="conteneur">
+                <div class="recap">
+                    <a href=#><p class="text-recap depart">Paris, France</p></a>
+                </div>
+                <div class="recap">
+                    <a href=#><p class="text-recap arrivee">Londres, Royaume-Uni</p></a>
+                </div>
+                <div class="recap voyageurs">
+                    <a href=#> <p class="text-recap">1 adulte, v </p></a>
+                </div>
+                <div class="recap date">
+                    <a href=#><p class="text-recap">28 Sept 2020 |  + Ajouter un retour</p></a>
+                </div>
+                
+                <div class="recap modifier">
+                    <a href=#><p class="bouton-peche">Modifier le trajet</p></a>
+                </div>
+            </div>
+        </div>
+        <div class="indice-carbone">
+            <div class="container">
+                <div class="indice positif">
+                    <p>Moins d’1 KG / pers. de CO2 émis</p>
+                </div>
+                <div class="indice moyen1">
+                    <p>Entre 2 KG et 25 KG / pers. de CO2 émis</p>
+                </div>
+                <div class="indice moyen2">
+                    <p>Entre 25 KG et 50 KG / pers. de CO2 émis</p>
+                </div>
+                <div class="indice negatif">
+                    <p>+ de 130 KG / pers.</p>
+                </div>
+            </div>
+        </div>
+
+        
+        
+        <section id="mode-transport">
+            <div class=container>
+                <div class="row" >
+                   
+                    <ul id="filtresTransport">
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Vélo</a>    
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Voile</a>
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Trains</a>
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Voiture électrique</a>
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Bus</a>
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Covoiturage</a>
+                        </li>
+                        <li class="filtre-transport">
+                            <a href="#" class="mode-transport">Vols</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="barre-selection">
+                    <div class="velo-actif selection"></div>
+                    <div class="voile-actif selection"></div>
+                    <div class="trains-actif selection"></div>
+                    <div class="voiture-actif selection"></div>
+                    <div class="bus-actif selection"></div>
+                    <div class="covoiturage-actif selection"></div>
+                    <div class="vols-actif selection"></div>
+                </div>
+            </div>
+            
+            
+        </section>
+
+        <section id="resultats-billets">
+            <div>
+                <h2>Autres Trajets<span style="color: #f0856b; font-size:1.5em;">.</span></h2>
+                <div class="les-billets">
+                    <img class="carte-trajet" src="img/carte-resultats.png" alt="carte itinéraire du trajet">
+                    <?php
+                            require 'admin/database.php';
+                            $db = Database::connect();
+                            $statement = $db->query('SELECT billets.id, billets.lieu_depart, billets.lieu_arrivee, billets.duree, billets.compagnie, billets.co2_emis, billets.pourcentage, billets.prix, billets.ld_recherche FROM billets 
+                            WHERE ld_recherche = "' . $_GET['villeDepart'] . '" AND la_recherche = "' . $_GET['villeArrivee'] . '"
+                            ORDER BY billets.id ASC');
+                            //$statement = $db->query('SELECT billets.id, billets.lieu_depart, billets.lieu_arrivee, billets.duree, billets.compagnie, billets.co2_emis, billets.pourcentage, billets.prix FROM billets WHERE ld_recherche = \'Paris, France\' ORDER BY billets.id ASC'); --> test
+                            //WHERE lieu depart et lieu arrivee = variables des champs sélectionnés pour la recherche + mode_transport = filtre.mode_transport --> test
+                            //echo print_r($statement);
+                            while($billet = $statement->fetch(PDO::FETCH_ASSOC)) 
+                            {
+                                //echo print_r($billet);
+                                echo '<div class="billet" id="billet1">';
+                                echo '<div class="horaires rub">';
+                                echo '<p class="p">'. $billet['lieu_depart'] . '</p>';
+                                echo '<p class="p">'. $billet['lieu_arrivee'] . '</p>';
+                                echo '</div>';
+                                echo '<div class="duree rub">';
+                                echo '<p class="p">'. $billet['duree'] . '</p>';
+                                echo '<p class="p">'. $billet['compagnie'] . '</p>';
+                                echo '</div>';
+                                echo '<div class="empreinteC rub">';
+                                echo '<p class="p">'. $billet['co2_emis'] . ' KG</p>';
+                                echo '<p class="p">'. $billet['pourcentage'] . '%</p>';
+                                echo '</div>';
+                                echo '<div class="infos rub">';
+                                echo '<p class="p">CO2 émis / personne pour un aller</p>';
+                                echo '<p class="p">Du quota annuel pour limiter le réchauffement</p>';
+                                echo '</div>';
+                                echo '<div class="prix rub5">';
+                                echo '<p class="p p9">'. $billet['prix'] . '€</p>';
+                                echo '<p class="p p10">Plus de détails</p>';
+                                echo '</div>';
+                                echo '</div>';
+                                //echo '<p> Ville de départ sélectionnée :'. $_GET['villeDepart'] . '___ </p>';
+                                //echo '<p> Ville d\'arrivée sélectionnée :'. $_GET['villeArrivee'] . '</p>';
+                            }
+                    ?>
+                    <!--
+
+                    <div class="billet" id="billet1">
+                        <div class="horaires rub">
+                            <p class="p">17:37   Paris Gare du Nord</p>
+                            <p class="p">20:38   Londres St Pancras</p>
+                        </div>
+                        <div class="duree rub">
+                            <p class="p">2h17   Direct</p>
+                            <p class="p">Eurostar</p>
+                        </div>
+                        <div class="empreinteC rub">
+                            <p class="p">3 kg</p>
+                            <p class="p">0.1%</p>
+                        </div>
+                        <div class="infos rub">
+                            <p class="p">CO2 émis / personne pour un aller</p>
+                            <p class="p">Du quota annuel pour limiter le réchauffement</p>
+                        </div>
+                        <div class="prix rub5">
+                            <p class="p p9">174€</p>
+                            <p class="p p10">Plus de détails</p>
+                        </div>
+                    </div>
+
+                    <div class="billet" id="billet2">
+                        <div class="horaires rub">
+                            <p class="p">17:37   Paris Gare du Nord</p>
+                            <p class="p">20:38   Londres St Pancras</p>
+                        </div>
+                        <div class="duree rub">
+                            <p class="p">2h07   Direct</p>
+                            <p class="p">Eurostar</p>
+                        </div>
+                        <div class="empreinteC rub">
+                            <p class="p">3 kg</p>
+                            <p class="p">0.1%</p>
+                        </div>
+                        <div class="horaires rub">
+                            <p class="p">CO2 émis / personne pour un aller</p>
+                            <p class="p">Du quota annuel pour limiter le réchauffement</p>
+                        </div>
+                        <div class="prix rub5">
+                            <p class="p p9">184€</p>
+                            <p class="p p10">Plus de détails</p>
+                        </div>
+                    </div>-->
+                </div>
+            </div>
+        </section>
+        <section class="footer">
+            <div class="container">
+                <div class="row">
+                    <div id="entreprise" class="bloc">
+                        <h5>L'entreprise</h5>
+                        <a href="#"><p>Conditions d'utilisation</p></a>
+                        <a href="#"><p>Nos valeurs écologiques</p></a>
+                        <a href="#"><p>Devenez partenaire</p></a>
+                        <a href="#"><p>Mentions légales</p></a>
+                        <a href="#"><p>CGU/CGV</p></a>
+                        <a href="#"><p>Contact</p></a>
+                    </div>
+                    <div id="voyage" class="bloc">
+                        <h5>Votre voyage</h5>  
+                        <a href="#"><p>Vos réservations</p></a>
+                        <a href="#"><p>Créer un compte</p></a>
+                        <a href="#"><p>Foire aux questions</p></a>
+                        <a href="#"><p>Avis de nos clients</p></a>
+                    </div>
+                    <div id="aide" class="bloc">
+                        <h5>Besoin d'aide</h5>
+                        <a href="#" class="bouton-aide bold bleu-fp">Centre d'aide</a>
+                    </div>
+                    <div id="nous-suivre">
+                        <div class="bloc">
+                            <h5>Nous suivre</h5>
+                            <a href="#"><img src="img/icon-facebook.PNG" alt="icone Facebook" /></a>
+                            <a href="#"><img src="img/icon-twitter.PNG" alt="icone Twitter" /></a>
+                            <a href="#"><img src="img/icon-instagram.PNG" alt="icone Instagram" /></a>
+                            <a href="#"><img src="img/icon-youtube.PNG" alt="icone Youtube" /></a>
+                            <a href="#"><img src="img/icon-linkedin.PNG" alt="icone Linkedin" /></a>
+                        </div>
+                        <br>
+                        <a  href="#"><img src="img/LOGO1.PNG" class="footer-brand" alt="logo Flying Papers" /></a>
+                    </div>
+                </div>      
+            </div>
+            <div style="background:pink;color:#333;position:fixed;right:0;bottom:0;z-index:99999999;font:1em arial;" id="ld"></div><script>setInterval(function(){if($(window).height()>=$(document).height()){$('#ld').text($(document).width()+' px');}else{$('#ld').text($(document).width()+17+' px');}},150);</script>
+
+        </section>
+    </body>
+</html>
